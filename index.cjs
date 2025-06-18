b1bcbae2e617000ec9a1cab025784fc91c38bdd6
Jader Golindano <golindano.jader@gmail.com>
2025-06-18 14:28:38 -0400

    first commit

---
+++ index.cjs
@@ -0,0 +1,37 @@
+
+const fs = require('fs');
+const cron = require('node-cron');
+const { exec } = require('child_process');
+
+
+const filePath = 'request.txt';
+
+cron.schedule('* * * * *', () => {
+
+    console.log('Ejecutando git commit...');
+
+    
+    //genero un commit con la fecha y hora actual
+    exec('cd C:/laragon/www/lavieja && git add . && git commit -m "Commit automático: ' + new Date().toISOString() + '"', (err, stdout, stderr) => {
+        if (err) console.error(`Error: ${err.message}`); });
+
+    console.log('Ejecutando git push...');
+    
+    exec('cd C:/laragon/www/lavieja && git push origin main', (err, stdout, stderr) => {
+        if (err) console.error(`Error: ${err.message}`);
+        if (stdout) console.log(`Salida: ${stdout}`);
+        if (stderr) console.error(`Error estándar: ${stderr}`);
+    
+    });
+
+   
+      
+       
+        fs.writeFile(filePath, new Date().toISOString(), function (err){})
+	   // fs.appendFile(filePath, count, function (err) {if (err) throw err;});
+	   
+
+
+});
+
+console.log('Tarea programada para ejecutarse cada minuto');
