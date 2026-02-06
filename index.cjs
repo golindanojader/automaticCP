9331177b867bd91eed374c254d49562bd590f73d
Jader Golindano <golindano.jader@gmail.com>
2026-02-06 15:06:00 -0400

    Commit automático: 2026-02-06T19:06:00.019Z

--- index.cjs
+++ index.cjs
@@ -1,49 +1,53 @@
+const fs = require("fs");
+const cron = require("node-cron");
+const { exec } = require("child_process");
 
-const fs = require('fs');
-const cron = require('node-cron');
-const { exec } = require('child_process');
+console.log("hola mundo");
 
+const filePath = "request.txt";
 
-const filePath = 'request.txt';
+cron.schedule("* * * * *", () => {
+  console.log("Ejecutando git commit...");
+  //agregar console log con la fecha de hoy
+  console.log("Fecha de hoy:", new Date().toISOString().split("T")[0]);
+  //
 
-cron.schedule('* * * * *', () => {
+  //genero un commit con la fecha y hora actual
+  exec(
+    'cd C:/laragon/www/script && git add . && git commit -m "Commit automático: ' +
+      new Date().toISOString() +
+      '"',
+    (err, stdout, stderr) => {
+      if (err) console.error(`Error: ${err.message}`);
+    },
+  );
 
-    console.log('Ejecutando git commit...');
+  console.log("Ejecutando git push...");
 
-    
-    //genero un commit con la fecha y hora actual
-    exec('cd C:/laragon/www/script && git add . && git commit -m "Commit automático: ' + new Date().toISOString() + '"', (err, stdout, stderr) => {
-        if (err) console.error(`Error: ${err.message}`); });
+  exec(
+    "cd C:/laragon/www/script && git push origin main",
+    (err, stdout, stderr) => {
+      if (err) console.error(`Error: ${err.message}`);
+      if (stdout) console.log(`Salida: ${stdout}`);
+      if (stderr) console.error(`Error estándar: ${stderr}`);
+    },
+  );
 
-    console.log('Ejecutando git push...');
-    
-    exec('cd C:/laragon/www/script && git push origin main', (err, stdout, stderr) => {
-        if (err) console.error(`Error: ${err.message}`);
-        if (stdout) console.log(`Salida: ${stdout}`);
-        if (stderr) console.error(`Error estándar: ${stderr}`);
-      
-    });
+  const token = generarToken();
 
-      const token = generarToken();
-
-       
-        fs.writeFile(filePath, new Date().toISOString() + token, function (err){})
-	   // fs.appendFile(filePath, count, function (err) {if (err) throw err;});
-	   
-
-
+  fs.writeFile(filePath, new Date().toISOString() + token, function (err) {});
+  // fs.appendFile(filePath, count, function (err) {if (err) throw err;});
 });
 
 function generarToken(longitud = 10000) {
-    const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()?-_=+[]{};:,.<>?あい';
-    let token = '';
+  const caracteres =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()?-_=+[]{};:,.<>?あい";
+  let token = "";
 
-    for (let i = 0; i < longitud; i++) {
-        const index = Math.floor(Math.random() * caracteres.length);
-        token += caracteres.charAt(index);
-    }
+  for (let i = 0; i < longitud; i++) {
+    const index = Math.floor(Math.random() * caracteres.length);
+    token += caracteres.charAt(index);
+  }
 
-    return token;
+  return token;
 }
-
-
